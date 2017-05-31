7b65e08746439ab80b80b27511ab823690864cc2
Jucheol Moon <opus18@Jucheols-iMac.local>
2017-05-31 10:24:54 -0500

    Initial Commit

---
+++ UrNotAlone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrNotAlone
+//
+//  Created by Jucheol Moon on 5/31/17.
+//  Copyright © 2017 Aspen Road Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
